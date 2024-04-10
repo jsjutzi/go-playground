7202bde4009ecb16fa91a02b6b15af292fee824e
Jack Jutzi <jackjutzi@Jacks-MBP.lan>
2024-04-09 21:10:56 -0400

    add support for POST

--- main.go
+++ main.go
@@ -26,10 +26,42 @@ func setupAPI() {
 
 func importsHandler(eventEmitter *utils.EventEmitter) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
-        // Subscribe to events
-        subscription := eventEmitter.Subscribe()
-        defer eventEmitter.Unsubscribe(subscription)
+        // Set headers for SSE
+        w.Header().Set("Content-Type", "text/event-stream")
+        w.Header().Set("Cache-Control", "no-cache")
+        w.Header().Set("Connection", "keep-alive")
 
-        // Handle other logic here
+        // Subscribe to events
+        sub := eventEmitter.Subscribe()
+        defer eventEmitter.Unsubscribe(sub)
+
+        // Write SSE events to the client
+        for event := range sub {
+            // Format the SSE event
+            message := "event: " + event.Name + "\n"
+            message += "data: " + event.Data + "\n\n"
+
+            // Write the message to the client
+            w.Write([]byte(message))
+            w.(http.Flusher).Flush()
+        }
+
+        // If this point is reached, it means the SSE subscription has ended
+        // Now handle the POST request body
+        if r.Method == http.MethodPost {
+            // Parse the request body
+            err := r.ParseForm()
+            if err != nil {
+                http.Error(w, "Failed to parse request body", http.StatusBadRequest)
+                return
+            }
+
+            // Access form values from the request body
+            // Syntax r.Form.Get("fieldName")
+            // Process the form data accordingly
+            // Return a response
+            w.WriteHeader(http.StatusOK)
+            w.Write([]byte("POST request handled successfully"))
+        }
     }
 }
